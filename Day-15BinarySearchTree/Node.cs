db097e84dc6f315405c141dfa5ae06a9f7b07aaf
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-04-09 17:51:23 +0530

    [Ronit]Added : Creating BStwith Tree Number

---
+++ Day-15BinarySearchTree/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_15BinarySearchTree
+{
+    internal class Node<T>
+    {
+        public T data; // data variable
+        public Node<T> left; // declaring a left node
+        public Node<T> right; // declaring a right node
+        public Node(T data) // Creating constructor having node data as parameter
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ Day-15BinarySearchTree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_15BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> root = new Node<int>(56);
+            BinarySearchTree<int> bst = new BinarySearchTree<int>(root);
+            bst.Insert(30);
+            bst.Insert(70);
+            Console.WriteLine("Inorder Traversal of given binary tree is : ");
+            bst.InOrderTraversal(root);
+            bst.TreeSize();
+            Console.ReadLine();
+        }
+    }
+}
